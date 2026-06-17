4af4edbee93955796b05d54b99db7e69ac075fbb
barnabas <barnabseshun@gmail.com>
2026-06-17 23:13:49 +0100

    Hide Navbar and Footer on admin and login pages

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
 const Layout = ({ children }) => {
+  const location = useLocation();
+  const isAdminPage = location.pathname.startsWith('/admin') || location.pathname === '/login';
+
   return (
     <div className="flex flex-col min-h-screen">
-      <Navbar />
+      {!isAdminPage && <Navbar />}
       <main className="flex-grow">
         {children}
       </main>
-      <Footer />
+      {!isAdminPage && <Footer />}
     </div>
   );
 };
